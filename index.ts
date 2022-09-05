e0ed1bd566582bf5a694cd9b3ebf3719bf2edd08
Ширяков Александр <a.shiryakov@foodplexgroup.ru>
2022-09-05 06:13:32 +0300

    add helmet

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import sassMiddleware from 'node-sass-middleware';
+import helmet from 'helmet';
 import router from './router';
 
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(
   sassMiddleware({
     src: path.resolve(__dirname, 'src/scss'),
